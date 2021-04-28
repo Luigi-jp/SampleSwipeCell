4806b97e764f85887f65a44d4fc20a2565a939c5
佐藤瑠偉史 <base.basketball0607@gmail.com>
2021-04-28 23:24:19 +0900

    TableCellを作成

--- SampleSwipeCell/ViewController.swift
+++ SampleSwipeCell/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "サンプル"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
 
